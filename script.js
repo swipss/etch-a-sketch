4fe0e28f88450e832f34fbc98cc94fd22f8b37cc
Sten V <vassiljevsten1@gmail.com>
2022-04-18 20:06:31 +0300

    Create grids

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 
 
-const container = document.querySelector('container')
+const container = document.querySelector('.container')
+
+const gridContainer = document.createElement('div')
+gridContainer.classList.add('grid-container')
+
+
+
+for (let i = 0; i < 256; i++) {
+const grid = document.createElement('div')
+grid.classList.add('grid')
+gridContainer.appendChild(grid)
+
+
+    
+}
+container.appendChild(gridContainer)
